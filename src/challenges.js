8dfd89a9b823fb426ac825fd2e72298f7c7f0141
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2021-12-02 18:54:19 -0300

    Desafio 7 e 8

--- src/challenges.js
+++ src/challenges.js
@@ -61,14 +61,34 @@ console.log(highestCount([-2 ,-2 ,-1]));
   
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1 , cat2) {
+  if ( Math.abs(mouse - cat1) < Math.abs (mouse -cat2)) {
+    return "cat1";
+  } else if ( Math.abs(mouse - cat1) > Math.abs (mouse -cat2)) {
+    return "cat2";
+  } else {
+    return "os gatos trombam e o rato foge";
+  }
 }
+console.log(catAndMouse(4 , 3 , 2 ));
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let resultado = [];
+  for (let i= 0 ; i < array.length ; i += 1 ) {
+    if (array[i] % 3 === 0 && array[i] % 5 === 0) {
+      resultado.push("fizzBuzz");
+    } else if (array[i] % 3 === 0 ) {
+      resultado.push("fizz");
+    } else if (array[i] % 5 === 0 ) {
+      resultado.push("buzz");
+    } else {
+      resultado.push("bug!");
+    }
+  }
+  return resultado
 }
+console.log(fizzBuzz([7 ,9, 3]));
 
 // Desafio 9
 function encode() {
