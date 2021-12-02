95647ba34d7ec3b7c3111b9fc531bdbb9b058d04
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2021-12-02 15:31:53 -0300

    Desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -9,9 +9,10 @@ function compareTrue(p1,p2) {
 console.log(compareTrue(false, false));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return ((base * height) / 2);
 }
+console.log(calcArea(51 , 1));
 
 // Desafio 3
 function splitSentence() {
