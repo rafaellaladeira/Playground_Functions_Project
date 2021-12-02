6f1f9db8c2d51bd3e4e3f37edc2bf200a1a65690
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2021-12-02 20:22:46 -0300

    Algumas correçoes do Lint

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,5 @@
 // Desafio 1
-function compareTrue(p1,p2) {
+function compareTrue(p1, p2) {
   if (p1 === true && p2 === true) {
     return true;
   } else {
@@ -12,7 +12,7 @@ console.log(compareTrue(false, false));
 function calcArea(base, height) {
   return (base * height) / 2;
 }
-console.log(calcArea(5 , 2));
+console.log(calcArea(5, 2));
 
 // Desafio 3
 function splitSentence(string) {
@@ -22,22 +22,22 @@ console.log(splitSentence("go trybe"));
 
 // Desafio 4
 function concatName(lista) {  
-  return lista[lista.length -1] + ", " + lista[0];
+  return lista[lista.length - 1] + ", " + lista[0];
   }
 console.log(concatName(['foguete', 'não', 'tem', 'ré']));
 
 // Desafio 5
-function footballPoints(wins , ties) {
+function footballPoints(wins, ties) {
   let pontos = wins * 3 + ties;
   return pontos;
 }
-console.log(footballPoints(0 , 0));
+console.log(footballPoints(0, 0));
 
 // Desafio 6
 function highestCount(numeros) {
-  let maiorNumero = numeros[0] ;
+  let maiorNumero = numeros[0];
   let quantidade = 0;
-  for (let i = 0 ; i < numeros.length ; i+=1){
+  for (let i = 0; i < numeros.length; i+=1) {
     if (numeros[i] > maiorNumero) {
       maiorNumero = numeros[i];
     }
@@ -48,30 +48,30 @@ function highestCount(numeros) {
   }
   return quantidade;
 }
-console.log(highestCount([-2 ,-2 ,-1]));
+console.log(highestCount([-2, -2, -1]));
   
 
 // Desafio 7
-function catAndMouse(mouse, cat1 , cat2) {
-  if ( Math.abs(mouse - cat1) < Math.abs (mouse -cat2)) {
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(mouse - cat1) < Math.abs (mouse -cat2)) {
     return "cat1";
-  } else if ( Math.abs(mouse - cat1) > Math.abs (mouse -cat2)) {
+  } else if (Math.abs(mouse - cat1) > Math.abs (mouse -cat2)) {
     return "cat2";
   } else {
     return "os gatos trombam e o rato foge";
   }
 }
-console.log(catAndMouse(4 , 3 , 2 ));
+console.log(catAndMouse(4, 3, 2));
 
 // Desafio 8
 function fizzBuzz(array) {
   let resultado = [];
-  for (let i= 0 ; i < array.length ; i += 1 ) {
+  for (let i= 0; i < array.length; i += 1) {
     if (array[i] % 3 === 0 && array[i] % 5 === 0) {
       resultado.push("fizzBuzz");
-    } else if (array[i] % 3 === 0 ) {
+    } else if (array[i] % 3 === 0) {
       resultado.push("fizz");
-    } else if (array[i] % 5 === 0 ) {
+    } else if (array[i] % 5 === 0) {
       resultado.push("buzz");
     } else {
       resultado.push("bug!");
@@ -79,7 +79,7 @@ function fizzBuzz(array) {
   }
   return resultado
 }
-console.log(fizzBuzz([7 ,9, 3]));
+console.log(fizzBuzz([7, 9, 3]));
 
 // Desafio 9
 function encode(palavra) {
@@ -122,7 +122,7 @@ function decode(palavra2) {
   }
   return resultado;
 }
-console.log(decode("b1n1n1n1"))
+console.log(decode("b1n1n1n1"));
 
 module.exports = {
   calcArea,
