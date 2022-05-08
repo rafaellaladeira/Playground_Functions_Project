325db835d25cf3a4ce78a18a6c50ec8f2e0fe711
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-05-07 21:36:33 -0300

    Remove itens de propriedade intelectual da trybe

--- src/challenges.js
+++ src/challenges.js
@@ -2,36 +2,30 @@
 function compareTrue(p1, p2) {
   if (p1 === true && p2 === true) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
-console.log(compareTrue(false, false));
 
 // Desafio 2
 function calcArea(base, height) {
   return (base * height) / 2;
 }
-console.log(calcArea(5, 2));
 
 // Desafio 3
 function splitSentence(string) {
-  return string.split(" ");
+  return string.split(' ');
 }
-console.log(splitSentence('go trybe'));
 
 // Desafio 4
-function concatName(lista) {  
-  return lista[lista.length - 1] + ", " + lista[0];
-  }
-console.log(concatName(['foguete', 'não', 'tem', 'ré']));
+function concatName(lista) {
+  return `${lista[lista.length - 1]}, ${lista[0]}`;
+}
 
 // Desafio 5
 function footballPoints(wins, ties) {
   let pontos = wins * 3 + ties;
   return pontos;
 }
-console.log(footballPoints(0, 0));
 
 // Desafio 6
 function highestCount(numeros) {
@@ -48,81 +42,76 @@ function highestCount(numeros) {
   }
   return quantidade;
 }
-console.log(highestCount([-2, -2, -1]));
-  
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if (Math.abs(mouse - cat1) < Math.abs (mouse -cat2)) {
-    return "cat1";
-  } else if (Math.abs(mouse - cat1) > Math.abs (mouse -cat2)) {
-    return "cat2";
-  } else {
-    return "os gatos trombam e o rato foge";
+  if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)) {
+    return 'cat1';
+  } if (Math.abs(mouse - cat1) > Math.abs(mouse - cat2)) {
+    return 'cat2';
   }
+  return 'os gatos trombam e o rato foge';
 }
-console.log(catAndMouse(4, 3, 2));
 
 // Desafio 8
 function fizzBuzz(array) {
   let resultado = [];
-  for (let i= 0; i < array.length; i += 1) {
+  for (let i = 0; i < array.length; i += 1) {
     if (array[i] % 3 === 0 && array[i] % 5 === 0) {
-      resultado.push("fizzBuzz");
+      resultado.push('fizzBuzz');
     } else if (array[i] % 3 === 0) {
-      resultado.push("fizz");
+      resultado.push('fizz');
     } else if (array[i] % 5 === 0) {
-      resultado.push("buzz");
+      resultado.push('buzz');
     } else {
-      resultado.push("bug!");
+      resultado.push('bug!');
     }
   }
-  return resultado
+  return resultado;
 }
-console.log(fizzBuzz([7, 9, 3]));
 
 // Desafio 9
 function encode(palavra) {
-  let resultado = "";
-  for (let i = 0; i < palavra.length; i += 1 ) {
-    if (palavra[i] == "a") {
+  let resultado = '';
+  for (let i = 0; i < palavra.length; i += 1) {
+    if (palavra[i] == 'a') {
       resultado += 1;
-    } else if (palavra[i] == "e") {
+    } else if (palavra[i] == 'e') {
       resultado += 2;
-    } else if (palavra[i] == "i") {
+    } else if (palavra[i] == 'i') {
       resultado += 3;
-    } else if (palavra[i] == "o") {
+    } else if (palavra[i] == 'o') {
       resultado += 4;
-    } else if (palavra[i] == "u") {
-     resultado += 5;
+    } else if (palavra[i] == 'u') {
+      resultado += 5;
     } else {
       resultado += palavra[i];
     }
   }
   return resultado;
 }
-console.log(encode("bananana"))
+console.log(encode('bananana'));
 
 function decode(palavra2) {
-  let resultado = "";
-  for (let i = 0; i < palavra2.length; i += 1 ) {
+  let resultado = '';
+  for (let i = 0; i < palavra2.length; i += 1) {
     if (palavra2[i] == 1) {
-      resultado += "a";
+      resultado += 'a';
     } else if (palavra2[i] == 2) {
-      resultado += "e";
+      resultado += 'e';
     } else if (palavra2[i] == 3) {
-      resultado += "i";
+      resultado += 'i';
     } else if (palavra2[i] == 4) {
-      resultado += "o";
+      resultado += 'o';
     } else if (palavra2[i] == 5) {
-     resultado += "u";
+      resultado += 'u';
     } else {
       resultado += palavra2[i];
     }
   }
   return resultado;
 }
-console.log(decode("b1n1n1n1"));
+console.log(decode('b1n1n1n1'));
 
 module.exports = {
   calcArea,
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,78 +1,68 @@
 // Desafio 10
 function techList(array, name) {
-    let array2 = [];
-    let array1 = array.sort();
+  let array2 = [];
+  let array1 = array.sort();
   for (let i = 0; i < array1.length; i += 1) {
-      let tecnologia = {
-        tech: array1[i],
-        name: name,
-      }
-      array2.push(tecnologia);
-    } 
-    if (array2.length === 0) {
-      return "Vazio!"
-    } else {
-        return array2.sort();
-      }
- }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
+    let tecnologia = {
+      tech: array1[i],
+      name,
+    };
+    array2.push(tecnologia);
+  }
+  if (array2.length === 0) {
+    return 'Vazio!';
+  }
+  return array2.sort();
+}
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber(numeros) {
-   if (numeros.length !== 11) {
-    return "Array com tamanho incorreto."; 
-   } 
+  if (numeros.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
   for (let i = 0; i < numeros.length; i += 1) {
-        let contador = 0;
-        for (let j = 1; j < numeros.length; j+= 1) {
-          if (numeros[i] === numeros[j]) {
-            contador += 1;
-          }
-        }
-      if (contador > 2) {
-        return "não é possível gerar um número de telefone com esses valores";
+    let contador = 0;
+    for (let j = 1; j < numeros.length; j += 1) {
+      if (numeros[i] === numeros[j]) {
+        contador += 1;
       }
-    }  
-    for (let index = 0; index < numeros.length; index +=1) {
-      if (numeros[index] < 0 || numeros[index] > 9) {
-        return "não é possível gerar um número de telefone com esses valores";
-      } 
+    }
+    if (contador > 2) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  for (let index = 0; index < numeros.length; index += 1) {
+    if (numeros[index] < 0 || numeros[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return `(${numeros[0]}${numeros[1]}) ${numeros[2]}${numeros[3]}${numeros[4]}${numeros[5]}${numeros[6]}-${numeros[7]}${numeros[8]}${numeros[9]}${numeros[10]}`;
+}
 
-    }  
-    return "(" + numeros[0] + numeros[1] + ") " + numeros[2] + numeros[3] + numeros[4] + numeros[5] + numeros[6] + "-" + numeros[7] + numeros[8] + numeros[9] + numeros[10];
-         }
-      
-     console.log(generatePhoneNumber([0, 21, 3, 4, 14, 2, 7, 8, 19, 9, 4]));
-            
+console.log(generatePhoneNumber([0, 21, 3, 4, 14, 2, 7, 8, 19, 9, 4]));
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  if (lineA > Math.abs(lineB - lineC) && lineA < lineB + lineC) {
-    return true;
-  } else if (lineB > Math.abs(lineA - lineC) && lineB < lineA + lineC) {
-    return true;
-  } else if (lineC > Math.abs(lineB - lineA) && lineC < lineB + lineA) {
-    return true;
-  } else {
-    return false;
-  }
+  if (lineA > Math.abs(lineB - lineC) && lineA < lineB + lineC) return true;
+  if (lineB > Math.abs(lineA - lineC) && lineB < lineA + lineC) return true;
+  if (lineC > Math.abs(lineB - lineA) && lineC < lineB + lineA) return true;
+  return false;
 }
-console.log(triangleCheck(10, 14, 8));
 
 // Desafio 13
 function hydrate(string) {
   let quantidade = 0;
   let contador = (/[1-9]/g);
-  let arrayString  = string.match(contador);
-  arrayString = arrayString.join("");
-   for (let i = 0; i < arrayString.length; i += 1) {
-     let numeros = Number(arrayString[i]);  
-    quantidade = quantidade + numeros;
-    } if (quantidade === 1) {
-        return quantidade + " copo de água";
-    } else {
-       return quantidade + " copos de água";
-      }
+  let arrayString = string.match(contador);
+  arrayString = arrayString.join('');
+  for (let i = 0; i < arrayString.length; i += 1) {
+    let numeros = Number(arrayString[i]);
+    quantidade += numeros;
+  } if (quantidade === 1) {
+    return `${quantidade} copo de água`;
+  }
+  return `${quantidade} copos de água`;
 }
 console.log(hydrate('5 cachaça, 5 cervejas e 1 copo de vinho'));
 
